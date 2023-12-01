72c260d2baac3f1e2e0af56600a5d0cd428fa059
Sary Zeaiter <saryzeaiter@gmail.com>
2023-12-01 16:48:32 +0100

    Add first create in sequelize sync

--- app.js
+++ app.js
@@ -2,72 +2,20 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
-const mockCoworkings = require('./mock-coworkings')
-const coworkings = require('./mock-coworkings')
+
+const { sequelize } = require('./db/sequelizeSetup')
 
 app.use(express.json())
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-   res.json('Hello World !')
+    res.json('Hello World !')
 })
 
+const coworkingRouter = require('./coworkingRoutes/coworkingRoutes')
 
-
-app.get('/api/coworkings', (req, res) => {
-
-   // Afficher la phrase : Il y a ... coworkings dans la liste. 
-   res.json(mockCoworkings)
-})
-
-
-
-app.get('/api/coworkings/:id', (req, res) => {
-   let result = mockCoworkings.find(el => el.id === parseInt(req.params.id))
-
-   if (!result) {
-       result = `Aucun élément ne correspond à l'id n°${req.params.id}`
-   }
-   res.json(result)
-})
-
-// implémenter le endpoint post qui renvoie une réposne "post fonctionne"
-app.post('/api/coworkings/', (req, res) => {
-
-   // Ajouter le coworking dans le tableau, en automatisant la génération d'un id. On récupère le dernier élément du tableau et on ajoute +1 à son id.
-   // let coworking = req.body
-
-   const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
-   
-   // let coworking = {id: newId, superficy : req.body.superficy, capacity : req.body.capacity, name: req.body.name}
-
-   // ... SPREAD OPERATOR
-   let newcoworking = { id: newId, ...req.body }
-   mockCoworkings.push(newcoworking)
-
-   // On renvoie un objet qui contient les proriétés message et data
-   // message: `Le coworking a bien été ajouté`
-   const result = { message: `Le coworking a bien été ajouté`, data: newcoworking }
-   res.json(result)
-})
-
-
-// Implement endpoint put coworkings with :id, with the corresponding request from in Postman
-
-app.put('/api/coworkings/:id', (req, res) => {
-   // 
-   const urlID = parseInt(req.params.id)
-   let coworkingToEdit = coworkings.find(el => el.id === urlID)
-
-   coworkingToEdit.superficy = req.body.superficy
-
-   const result = { message : 'Coworkings Modified', data: coworkingToEdit}
-   res.json(result)
-})
-
-
-
+app.use('/api/coworkings', coworkingRouter)
 
 app.listen(port, () => {
-   console.log(`Example of app listening on port ${port}`)
-}) 
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ db/sequelizeSetup.js
@@ -0,0 +1,33 @@
+// B. On importe le gabarit du Model Coworking défini dans le fichier ./models/coworking'
+const CoworkingModel = require('../models/coworking')
+const { Sequelize, DataTypes } = require('sequelize');
+const mockCoworkings = require ('../mock-coworkings');
+const coworking = require('../models/coworking');
+
+// A. On créé une instance de bdd qui communique avec Xampp 
+const sequelize = new Sequelize('bordeaux_coworkings', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    logging: false
+});
+
+// C. On instancie un Model qui permettra d'interpréter le Javascript avec la Table SQL correspondante
+const Coworking = CoworkingModel(sequelize, DataTypes)
+
+// D. On synchronise la BDD avec les models défini dans notre API
+sequelize.sync({ force: true })
+    .then(() => {
+        mockCoworkings.forEach((element, index) => {
+                const newCoworking = {... element}
+                Coworking.create(newCoworking)
+        })
+
+    })
+
+
+sequelize.authenticate()
+    .then(() => console.log('La connexion à la base de données a bien été établie.'))
+    .catch(error => console.error(`Impossible de se connecter à la base de données ${error}`))
+
+
+module.exports = { sequelize, Coworking }
